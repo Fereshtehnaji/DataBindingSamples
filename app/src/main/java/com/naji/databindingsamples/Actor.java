a3e8c1ebc581c98e6e970f3d6f8ed138beb4f745
Fereshteh Naji <fereshteh.naji.ad@gmail.com>
2020-06-28 23:47:41 +0430

    add Data Binding to project

---
+++ app/src/main/java/com/naji/databindingsamples/Actor.java
@@ -0,0 +1,46 @@
+package com.naji.databindingsamples;
+
+public class Actor {
+    private int id;
+    private String name;
+    private String bio;
+    private int imageId;
+
+    public Actor(String name, String bio, int imageId) {
+        this.name = name;
+        this.bio = bio;
+        this.imageId = imageId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+}
--- app/src/main/java/com/naji/databindingsamples/MainActivity.java
+++ app/src/main/java/com/naji/databindingsamples/MainActivity.java
@@ -1,14 +1,24 @@
 package com.naji.databindingsamples;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.naji.databindingsamples.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        Actor actor = new Actor(getString(R.string.actor1_name),
+                getString(R.string.actor1_bio), R.drawable.actor_peyman_maadi);
+
+        mBinding.setActor(actor);
+
     }
 }
